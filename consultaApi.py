0e1097c9e19bdf080723c6d4fd3d2fd2d8bde6a3
Pedro Vilbert <pedrovilbert2005@gmail.com>
2025-08-13 17:33:57 -0300

    Avanços para gerar o mapa

--- consultaApi.py
+++ consultaApi.py
@@ -4,31 +4,46 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Iniciar navegador
+# Iniciar o navegador Chrome
 navegador = webdriver.Chrome()
+# Acessar a página de horóscopos do site astro.com
 navegador.get("https://www.astro.com/horoscope")
+# Maximizar a janela do navegador para melhor visualização
 navegador.maximize_window()
 
+# Definir uma espera explícita de até 10 segundos para encontrar elementos na página
 wait = WebDriverWait(navegador, 10)
 
-# Esperar e clicar no botão do menu
+# Esperar o botão do menu ficar clicável e clicar nele
 menu_botao = wait.until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, "img.navlink.navbar-toggler-left"))
 )
 menu_botao.click()
 
+# Esperar o botão "Free Horoscopes" ficar clicável e clicar nele
 bnt_free_horoscopes = wait.until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, ".xrubrik.rubrik_0"))
+    EC.element_to_be_clickable((By.CSS_SELECTOR, ".rubrik.rubrik_0"))
 )
 bnt_free_horoscopes.click()
+
+# Alternativa comentada: procurar todos os botões e clicar no que tem o texto "Free Horoscopes"
 # lista_botoes_menu = navegador.find_elements(By.CSS_SELECTOR, "rubrik.rubrik_0")
 # for botao in lista_botoes_menu:
 #     if "Free Horoscopes"== botao.text:
 #         botao.click()
 
-lista2_botoes_menu = navegador.find_elements("class name", "inner show")
+# Esperar o botão "Horoscope Drawings & Data" ficar clicável e clicar nele
+bnt_drawings_data = wait.until(
+    EC.element_to_be_clickable((By.XPATH, "//*[text()='Horoscope Drawings & Data']"))
+)
+bnt_drawings_data.click()
 
+# Esperar o botão "Extended Chart Selection" ficar clicável e clicar nele
+bnt_chart_selection = wait.until(
+    EC.element_to_be_clickable((By.XPATH, "//*[text()='Extended Chart Selection']"))
+)
+bnt_chart_selection.click()
 
+# Esperar 10 segundos para visualizar o resultado antes de fechar o navegador
 time.sleep(10)
-navegador.quit()
-
+# Fechar o navegador
